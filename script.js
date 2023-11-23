db2ad3ac34b492794e40f492be777349a779bc09
geancampos <gean699@hotmail.com>
2023-11-23 12:51:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -106,13 +106,13 @@ function iniciarOuPausar() {
     inicioContador.play();
     intervaloId = setInterval(contagemRegressiva, 1000);
     iniciarOuPausarBt.textContent = "Pausar"
-    iniciarOuPausarBtIcone.setAttribute('src', `/imagens/pause.png`)
+    iniciarOuPausarBtIcone.setAttribute('src', `imagens/pause.png`)
 }
 
 function zerar() {
     clearInterval(intervaloId)
     iniciarOuPausarBt.textContent = "Começar"
-    iniciarOuPausarBtIcone.setAttribute('src', `/imagens/play_arrow.png`)
+    iniciarOuPausarBtIcone.setAttribute('src', `imagens/play_arrow.png`)
     intervaloId = null
 }
 
